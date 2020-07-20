45826ebc6f5ca6e1e80c785a728788ac6912049d
shridhar.1si11ec106@gmail.com <shridhar.1si11ec106@gmai.com>
2020-07-20 18:20:12 +0530

    Commit 1

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d3365971-675d-4d95-b4d0-9089760258b3</testSuiteGuid>
+   <testCaseLink>
+      <guid>d3e18f08-ffcf-47d3-90c4-03f300160f6b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1 - Copy</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e2b4f37b-3b9f-4204-a9ad-b845db8447c4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
